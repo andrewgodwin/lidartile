e6f008e69e23908482325172b42a38e08ca98c12
Andrew Godwin <andrew@aeracode.org>
2016-02-28 11:36:52 -0800

    Make smoothing optional

--- lidartile/cli.py
+++ lidartile/cli.py
@@ -10,13 +10,13 @@ from .stlwriter import StlWriter
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-d", "--divisor", type=int, default=1)
-    parser.add_argument("-f", "--featuresize", type=float, default=0.3)
+    parser.add_argument("-f", "--featuresize", type=float, default=0)
     parser.add_argument("-b", "--base", type=float, default=10)
     parser.add_argument("-c", "--clip", type=float, default=5)
     parser.add_argument("-z", "--zboost", type=float, default=1)
     parser.add_argument("-s", "--snap", type=float, default=0)
     parser.add_argument("-e", "--snapexp", type=float, default=0)
-    parser.add_argument("-m", "--smoothing", type=float, default=1)
+    parser.add_argument("-m", "--smoothing", type=float, default=0)
     parser.add_argument("-l", "--scale", type=float, default=0.3)
     parser.add_argument("files", nargs="+")
     args = parser.parse_args()
@@ -24,8 +24,9 @@ def main():
     ingestor = AscIngestor(args.files, divisor=args.divisor, clip=-args.clip, zboost=args.zboost, snap=args.snap, snapexp=args.snapexp)
     ingestor.load()
     grid = ingestor.grid
-    print "Smoothing..."
-    grid.smooth(factor=args.smoothing)
+    if args.smoothing:
+        print "Smoothing..."
+        grid.smooth(factor=args.smoothing)
     if args.featuresize:
         optimiser = GridOptimiser(ingestor.grid, delta=args.featuresize)
         print "Optimising..."
